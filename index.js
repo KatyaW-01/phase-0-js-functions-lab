d508404797d5652be4945a88e2245ec41efd776a
Katya Weicht <katya.weicht@gmail.com>
2025-05-07 15:36:50 -0600

    Write all functions

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+function calculateTax(amount){
+  return amount * 0.1
+}
 
+function convertToUpperCase(text){
+  return text.toUpperCase()
+}
+
+function findMaximum(num1,num2){
+  if(num1>num2){
+    return num1
+  }else{
+    return num2
+  }
+}
+
+function isPalindrome(word){
+  if(word === word.split("").reverse().join("")){
+    return true
+  }else{
+    return false
+  }
+}
+
+function calculateDiscountedPrice(originalPrice,discountPercentage){
+  let discount = originalPrice * (discountPercentage * 0.01)
+  return originalPrice - discount
+}
 
 
 
